fa458f8431cf88ecb7b10cfede6a7183f9c61342
steste375 <steevensonlaguerre89@gmail.com>
2025-08-30 18:50:26 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;qckRyAqK#lv-pBoczfEPlIz1X019-I3UupqdWbqJ38sKjl4xHFyM", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "50936517758", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
